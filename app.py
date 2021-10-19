0407b3698ed5c38fb4a41a45fb9f1baf762ff9cb
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-10-20 01:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,11 @@ from markupsafe import escape
 import os
 import threading
 
-app = Flask("needmedi")
-app.debug = True
-
-
-def build(branch):
-    print(" build started")
-    os.system(f"./build.sh {escape(branch)}")
-    print(" build over")
-
+app = Flask(__name__)
 
 @app.route('/app/<branch>')
 def main(branch):
-    threading.Thread(target=build, args=(branch,)).start()
+     os.system(f"./pull.sh")
     return "Building job started. Please wait this may take some time"
 
 if __name__ == '__main__':
