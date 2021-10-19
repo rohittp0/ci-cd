0f8aaacd9db9b72404127cc596a48d441131c33c
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-10-20 01:50:35 +0530

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/user/hopital-availabilty-frontend
+
+UPSTREAM=${1:-'@{u}'}
+DIFFCOMM=$(git fetch origin --quiet; git rev-list HEAD..."$UPSTREAM" --count)
+if [ "$DIFFCOMM" -gt 0 ]; then
+  echo "Pulling $UPSTREAM" >> /home/user/update_log
+  git pull
+else
+  echo "Upto date" >> /home/user/update_log
+fi
