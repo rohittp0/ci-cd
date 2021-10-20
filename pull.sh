de46003a82d6dd240892a90828d7cdca3877c29c
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-10-20 21:33:39 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -7,6 +7,7 @@ DIFFCOMM=$(git fetch origin --quiet; git rev-list HEAD..."$UPSTREAM" --count)
 if [ "$DIFFCOMM" -gt 0 ]; then
   echo "Pulling $UPSTREAM" >> /home/user/update_log
   git pull
+  yarn build
 else
   echo "Upto date" >> /home/user/update_log
 fi
