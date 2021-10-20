fe0c4e47a694d5614c2d01865af1ec77d85611e2
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-10-20 21:58:46 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -9,5 +9,29 @@ if [ "$DIFFCOMM" -gt 0 ]; then
   git pull
   yarn build
 else
-  echo "Upto date" >> /home/user/update_log
+  echo "master Upto date" >> /home/user/update_log
+fi
+
+cd /home/user/hopital-availabilty-frontend-nidhin
+
+UPSTREAM=${1:-'@{u}'}
+DIFFCOMM=$(git fetch origin --quiet; git rev-list HEAD..."$UPSTREAM" --count)
+if [ "$DIFFCOMM" -gt 0 ]; then
+  echo "Pulling $UPSTREAM" >> /home/user/update_log
+  git pull
+  yarn build
+else
+  echo "nidhin Upto date" >> /home/user/update_log
+fi
+
+cd /home/user/hopital-availabilty-frontend-sanu
+
+UPSTREAM=${1:-'@{u}'}
+DIFFCOMM=$(git fetch origin --quiet; git rev-list HEAD..."$UPSTREAM" --count)
+if [ "$DIFFCOMM" -gt 0 ]; then
+  echo "Pulling $UPSTREAM" >> /home/user/update_log
+  git pull
+  yarn build
+else
+  echo "sanu Upto date" >> /home/user/update_log
 fi
