b6fc0b82b266b06979a561fadc85d78601b25cf7
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-10-23 23:34:09 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -4,15 +4,20 @@ pull_and_build()
 {
   if [ -d ~/hopital-availabilty-frontend-${1} ]; then
     cd ~/hopital-availabilty-frontend-$1
+    echo "Switched to ~/hopital-availabilty-frontend-${1}"
   else
     git clone https://github.com/Trebuchet-ltd/hopital-availabilty-frontend ~/hopital-availabilty-frontend-${1}
     cd ~/hopital-availabilty-frontend-$1
     git switch -c $1 || git switch $1
-    git branch --set-upstream-to origin/$1
   fi
+  
+   [ ! $(git status -b --porcelain=v2 | grep -m 1 "^# branch.upstream
+ " | cut -d " " -f 3-) ] && git branch --set-upstream-to origin/$1
   
   UPSTREAM=${1:-'@{u}'}
   DIFFCOMM=$(git fetch origin --quiet; git rev-list HEAD..."$UPSTREAM" --count)
+  
+  echo "Found ${DIFFCOMM} changes"
   
   if [ "$DIFFCOMM" -gt 0 ]; then
     echo "<-- Pulling ${1}" >> /home/user/update_log
