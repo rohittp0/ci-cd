3f4cf6ffaf57fe9bc8b5ec58d9fd44683980ed68
Rohit T P <tprohit9@gmail.com>
2021-11-09 19:03:40 +0530

    added branch auto recreate

--- branch/urls.py
+++ branch/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from branch.views import on_delete
 
 urlpatterns = [
-    path(r'on_delete', on_delete),
+    path(r'on_delete/', on_delete),
 ]
--- branch/views.py
+++ branch/views.py
@@ -1,8 +1,31 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
 from rest_framework.decorators import api_view
 
+from rest_framework.response import Response
 
-# Create your views here.
+
 @api_view(["POST"])
 def on_delete(request):
-    print(request.data)
-    # default_branch
+    load_dotenv()
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'token {os.getenv("GIT_HUB_TOKEN")}'
+    }
+
+    repo_name = request.data['repository']['full_name']
+
+    repo = requests.get(f"https://api.github.com/repos/{repo_name}", headers=headers).json()
+
+    base = requests.get(f"https://api.github.com/repos/{repo_name}/git/refs/heads/{repo['default_branch']}",
+                        headers=headers).json()
+
+    requests.post(f"https://api.github.com/repos/{repo_name}/git/refs",
+                  headers=headers,
+                  data=json.dumps({"ref": f"refs/heads/{request.data['ref']}", "sha": base["object"]["sha"]}))
+
+    return Response(200)
